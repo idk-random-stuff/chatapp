be125a81f2b4e2ee0184ced79026f213b6b150ef
idk-random-stuff <149273480+idk-random-stuff@users.noreply.github.com>
2024-11-11 17:16:49 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public')); // Serve static files from the 'public' folder
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast the message to all clients
+  });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
